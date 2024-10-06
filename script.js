ff5fb24c8e38bc98bf2e0935ad42761d76489c05
shreyansh090205 <syeole090205@gmail.com>
2024-10-06 23:53:11 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("search-btn");
+
+btn.addEventListener("click", () => {
+    let inpWord = document.getElementById("inp-word").value;
+
+    fetch(`${url}${inpWord}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+
+            const phonetic = data[0].phonetics.find(p => p.audio); // Find phonetics entry with audio
+            const audioUrl = phonetic ? phonetic.audio : null;
+
+            result.innerHTML =
+                `<div class="word">
+                    <h3>${inpWord}</h3>
+                    <button onclick = "playSound()">
+                        <i class="fa-solid fa-volume-high"></i>
+                    </button>
+                </div>
+                <div class="details">
+                    <p>${data[0].meanings[0].partOfSpeech}</p>
+                    <p>/${data[0].phonetic}/</p>
+                </div>
+                <p class="word-meaning">
+                    ${data[0].meanings[0].definitions[0].definition}
+                </p>
+                <p class="word-example">
+                ${data[0].meanings[0].definitions[0].example || "No exxample available for this word"}
+                </p>
+            `;
+
+            if (audioUrl) {
+                sound.setAttribute("src", audioUrl);
+            } else {
+                console.log("No audio available for this word.");
+            }
+        })
+        .catch(() => {
+            // console.error('Error:');
+            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+        });
+
+});
+function playSound() {
+    sound.play();
+}
